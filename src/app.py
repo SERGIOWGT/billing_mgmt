9bc5414bbff45767b0bd526cf030c94eb29a14fd
sergiowgt <sergiowgt@gmail.com>
2023-01-22 09:29:51 -0300

    melhorias

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
 from dataclasses import dataclass
 from .email_handler import EmailHandler
 
-
 @dataclass
 class App:
     @staticmethod
     def execute(smtp_server: str, user_name: str, password: str, download_folder: str)->None:
-        email = EmailHandler(smtp_server, user_name, password, download_folder) 
+        email = EmailHandler(download_folder)
+        
+        email.login(smtp_server, user_name, password, use_ssl=True)
+        messages_id = email.get_messages_id('INBOX')
+        
+        for message_uid in messages_id:
+            files = email.get_files()
+                
+            if (files):
+                print('salva')
+        
 
-        email.execute()
+        #email.execute()
--- src/email_handler.py
+++ src/email_handler.py
@@ -5,92 +5,93 @@ from email import message
 import imaplib
 import os
 import re
+from typing import List
 #import sys
 from .exception_handler import ApplicationException
 
 
 @dataclass
 class EmailHandler:
-    smtp_server = ''
-    user_name = ''
-    password = ''
+    imap_session: imaplib.IMAP4_SSL = None
     download_folder= ''
 
-    def __init__(self, imap_server, user_name: str, password: str, download_folder) -> None:
-        ApplicationException.when(not isinstance(imap_server, str), 'IMAP Server invalid')
-        ApplicationException.when(not isinstance(user_name, str), 'User name invalid')
-        ApplicationException.when(not isinstance(password, str), 'Password invalid')
-        imap_server.strip()
+    def __init__(self, download_folder) -> None:
+        self.download_folder = download_folder
+
+    def login(self, host: str, user_name: str, password: str, use_ssl: True) -> None:
+        ApplicationException.when(not isinstance(
+            host, str), 'Host invalid')
+        ApplicationException.when(not isinstance(
+            user_name, str), 'User name invalid')
+        ApplicationException.when(not isinstance(
+            password, str), 'Password invalid')
+        host.strip()
         user_name.strip()
         password.strip()
-        ApplicationException.when(imap_server == '', 'IMAP Server not specified')
+        ApplicationException.when(
+            host == '', 'Host not specified')
         ApplicationException.when(user_name == '', 'User name not specified')
         ApplicationException.when(password == '', 'Password not specified')
-
-        self.imap_name = imap_server
-        self.user_name = user_name
-        self.password = password
-        self.download_folder = download_folder
         
-    def get_email_infos(self, email_message: message.Message) -> tuple:
-        subject = ''
-        sender = ''
-        rec_date = ''
-
-        for el in email_message._headers:
-            key = el[0].upper()
-            if (key == 'FROM'):
-                sender = el[1]
-            elif (key == 'DATE'):
-                rec_date = el[1]
-            elif (key == 'SUBJECT'):
-                subject = el[1]
-
-        return subject, sender, rec_date
-
-    def execute(self)->None:
-        imap_session = imaplib.IMAP4_SSL(self.imap_name)
-        imap_session.ssl = True
-        result, account_details = imap_session.login(self.user_name, self.password)
+        self.imap_session = imaplib.IMAP4_SSL(host)
+        self.imap_session.ssl = use_ssl
+        
+        result, account_details = self.imap_session.login(user_name, password)
         ApplicationException.when(result != 'OK', 'Not able to sign in!')
 
-        imap_session.select('INBOX')
-        result, messages = imap_session.uid('search', None, "ALL")
+
+    def get_messages_id(self, folder: str)->List[int]:
+        self.imap_session.select(folder)
+        result, messages = self.imap_session.uid('search', None, "ALL")
         ApplicationException.when(result != 'OK', 'Error searching Inbox.')
-        
-        messages_id = messages[0].split()
 
-        for message_id in messages_id:
-            result, email_data = imap_session.uid('fetch', message_id, '(RFC822)')
-            raw_email = email_data[0][1]
+        return messages[0].split()
 
-            # converts byte literal to string removing b''
-            raw_email_string = raw_email.decode('latin-1')
-            email_message = email.message_from_string(raw_email_string)
+    
+"""  def execute(self)->None:
+self.imap_session.select('INBOX')
+result, messages = imap_session.uid('search', None, "ALL")
+ApplicationException.when(result != 'OK', 'Error searching Inbox.')
 
-            # downloading attachments
-            for part in email_message.walk():
-                # this part comes from the snipped I don't understand yet... 
-                if part.get_content_maintype() == 'multipart':
-                    continue
-                if part.get('Content-Disposition') is None:
-                    continue
-                att_file_name = part.get_filename()
-                if (att_file_name):
-                    att_file_name = re.sub("[:\\/\s]", "_", att_file_name)
-                else:
-                    att_file_name = 'sem_nome'
-                    
-                    
+messages_id = messages[0].split()
 
-                if bool(att_file_name):
-                    download_path = f"{self.download_folder}/{att_file_name}"
-                    if not os.path.isfile(download_path) :
-                        with open(download_path, 'wb') as file:
-                            file.write(part.get_payload(decode=True))
+for message_id in messages_id:
+    result, email_data = imap_session.uid('fetch', message_id, '(RFC822)')
+    raw_email = email_data[0][1]
+
+    # converts byte literal to string removing b''
+    raw_email_string = raw_email.decode('latin-1')
+    email_message = email.message_from_string(raw_email_string)
+
+    # downloading attachments
+    for part in email_message.walk():
+        # this part comes from the snipped I don't understand yet... 
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+        att_file_name = part.get_filename()
+        if (att_file_name):
+            att_file_name = re.sub("[:\\/\s]", "_", att_file_name)
+        else:
+            att_file_name = 'sem_nome'
             
-            (subject, sender, rec_date) = self.get_email_infos(email_message)
-                                    
-            print(f'Downloaded "{att_file_name}" from "{sender}" titled "{subject}" on {rec_date}.')
+            
 
-        imap_session.logout()
+        if bool(att_file_name):
+            download_path = f"{self.download_folder}/{att_file_name}"
+            if not os.path.isfile(download_path) :
+                with open(download_path, 'wb') as file:
+                    file.write(part.get_payload(decode=True))
+    
+    (subject, sender, rec_date) = self.get_email_infos(email_message)
+                            
+    print(f'Downloaded "{att_file_name}" from "{sender}" titled "{subject}" on {rec_date}.')
+
+imap_session.logout()
+
+
+message_uid
+
+
+"""
